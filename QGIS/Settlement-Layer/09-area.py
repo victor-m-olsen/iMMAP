5d5feecffea9e4ba24036c3745517dd11cac197f
Victor Olsen <51537091+victor-m-olsen@users.noreply.github.com>
2024-01-23 10:55:25 +0200

    Delete QGIS directory

--- QGIS/Settlement-Layer/09-area.py
+++
@@ -1,9 +0,0 @@
-import geopandas as gpd
-
-os.chdir(r'D:\iMMAP\proj\ASDC\data\sett_workspace_v02')
-
-polygons = gpd.read_file(r'process\count.shp')
-
-polygons["area"] = polygons['geometry'].area
-
-polygons.to_file(r'output\pop-build-area.shp')
--- QGIS/Settlement-Layer/10-column-names.py
+++
@@ -1,7 +0,0 @@
-import geopandas as gpd
-
-os.chdir(r'D:\iMMAP\proj\ASDC\data\sett_workspace_v02')
-
-layer = gpd.read_file(r'output\pop-build-area.shp')
-
-print(layer.head())
